06f01451194f86ae083b7a7b97d6fc14ab24737f
Maryam <sshah073@ucr.edu>
2024-04-03 10:52:30 -0700

    prunning boundaries updated to cover the full sequence for all data points

--- momp.py
+++ momp.py
@@ -23,39 +23,47 @@ import warnings
 
 def momp(T, m, dd):
 
-    bsf = np.inf
-    momp_out, amp, absf, bsf, _ = process(T, m, dd, np.arange(len(T)), bsf)
+    bsf, bsf_loc = np.inf,None
+    idxList = np.arange(len(T))
+    while dd > 0.5:
+        T, idxList, bsf, bsf_loc = process(T, m, dd, idxList, bsf, bsf_loc)
+        dd = dd // 2
+        
 
-    return momp_out, amp, absf, bsf
-
-def process(T, m, dd, idxList, bsf):
-
+def process(T, m, dd, idxList, bsf, bsf_loc):
     # idxList : original indices for the input data points
-    amp, _ , _ , _ = approxMP(T, m, dsr = dd)
-    absf, original_bsf_loc= bsfMotif(amp, m)
-    bsf,momp_out, original_bsf = exactLocalSearch(T,m, dd, original_bsf_loc, bsf, idxList)
-    pruned_ts , idxList, prunning_perc = prune(T, m, absf, bsf, amp, idxList, n_orig)
+    amp  = approxMP(T, m, dd)
+    absf, absf_loc= bsfMotif(amp)
+    if dd > 1:
+        bsf,bsf_loc, bsf_origin = exactLocalSearch(T,m, dd, absf_loc, bsf, bsf_loc, idxList)
+        bsf_loc_origin = idxList[absf_loc]
+        pruned_T , pruned_idxList = prune(T, m, absf, bsf, amp, idxList)
+        pruning_perc = round(1- (len(pruned_T) / n_orig), 3)
+        print('MOMP: Tpaa1in{} |BSFdist : {} |Loc: {} |'\
+            'OriginBSFdist: {} |OriginBSFloc: {} |PrunePerc: {}'\
+                .format(dd, bsf, bsf_loc,\
+                            bsf_origin, bsf_loc_origin,\
+                            pruning_perc))
+        return pruned_T, pruned_idxList, bsf, bsf_loc
+    else:
+        pruning_perc = round(1- (len(T) / n_orig), 3)
+        absf, absf_loc = round(absf,2), idxList[absf_loc]
+        print('MOMP: Tpaa1in{} |BSFdist : {} |Loc: {} |'\
+            'OriginBSFdist: {} |OriginBSFloc: {} |PrunePerc: {}'\
+                .format(dd, absf, absf_loc,\
+                            absf, absf_loc,\
+                            pruning_perc))
+        return T, idxList, absf, absf_loc
 
-    print('MOMP: Tpaa1in{} |BSFdist : {} |Loc: {} |'\
-          'OriginBSFdist: {} |OriginBSFloc: {} |PrunePerc: {}'\
-            .format(dd, round(bsf,2), momp_out,\
-                round(original_bsf,2), original_bsf_loc,\
-                                        round(prunning_perc,4)))
 
-    if dd//2 >= 4:
-       momp_out, amp, absf, bsf, pruned_ts = process(pruned_ts, m, dd//2, idxList, bsf)
-
-    return momp_out, amp, absf, bsf, pruned_ts
-
-
-def findMaxPow2Sub(N):
+# def findMaxPow2Sub(N):
  
-    # if N is a power of two simply return it
-    if (not (N & (N - 1))):
-        return N
+#     # if N is a power of two simply return it
+#     if (not (N & (N - 1))):
+#         return N
          
-    # else set only the most significant bit
-    return 0x8000000000000000 >>  (64 - N.bit_length())
+#     # else set only the most significant bit
+#     return 0x8000000000000000 >>  (64 - N.bit_length())
 
 def next_closest_multiple_greater(N, K):
     quotient, remainder = divmod(N, K)
@@ -69,14 +77,14 @@ def main():
 
     plt.close('all')
 
-    T , m_orig = genData() , 800
+    T , m = genData(dtype='random') , 256
     global n_orig
     n_orig = len(T)
-    m = findMaxPow2Sub(m_orig)
+    #m = findMaxPow2Sub(m_orig)
     dd = m // 4
 
-    print('T length: {} | Requested motif length : {}'.format(n_orig, m_orig))
-    print('The largest power-of-two <  {} : {}'.format(m_orig, m))
+    print('T length: {} | Requested motif length : {}'.format(n_orig, m))
+    # print('The largest power-of-two <  {} : {}'.format(m_orig, m))
     print('Starting downsampling rate (1/4 * {}):  {}'.format(m, m//4))
 
     
@@ -86,17 +94,18 @@ def main():
 
     #Evaluation of the result
     st = time.time()
+    global mp
     mp = mpx.compute(T,m)['mp']
-    _, min_loc = bsfMotif(mp, m)
+    _, min_loc = bsfMotif(mp)
     mp_time = round(time.time() - st,2)
     print('MPx: {} | minval : {}'.format(min_loc, round(np.min(mp),2)))
 
     st = time.time()
-    _, amp, absf, bsf = momp(T, m, dd)
+    momp(T, m, dd)
     momp_time = round(time.time() - st,2)
 
     print('Speedup : {}X | MPx:{}s | MOMP: {}s'.format(round(mp_time / momp_time), mp_time, momp_time))
-    plotResult(T, m, mp, amp, absf, bsf, dd)
+    # plotResult(T, m, mp, amp, absf, bsf, dd)
 
 
 if __name__=="__main__":
--- momp_utils.py
+++ momp_utils.py
@@ -3,38 +3,34 @@ import time
 import numpy as np
 import matrixprofile as mpx
 from paa import paa
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
 def approxMP(ts, m, dsr):
-    st = time.time()
     if dsr > 1 : 
         ts_dsmp = paa(ts, len(ts)//dsr)
         w_dsmp = np.ceil(m/dsr).astype(int)
         dsmp_mp = mpx.compute(ts_dsmp, w_dsmp)['mp']
         amp = np.sqrt(dsr) * np.repeat(dsmp_mp, dsr)
-        end = time.time()
-        return amp , dsr, ts_dsmp, end-st
+        return amp
     else:
-        end = time.time()
-        return mpx.compute(ts,m)['mp'], 1, ts, end-st
+        return mpx.compute(ts,m)['mp']
 
 
-def prune(T, m, absf, bsf, amp, idxList, n_orig):
-    idx = findIndices(amp, m, absf, bsf)
-    pruned_ts, idxList = T[idx], idxList[idx]
-    remaining_perc = len(pruned_ts) / n_orig
-    return pruned_ts, idxList, 1- round(remaining_perc,4)
+def prune(T, m, absf, bsf, amp, idxList):
+    idx = findIndices(amp, m, absf, bsf, len(T))
+    pruned_ts, pruned_idxList = T[idx], idxList[idx]
+    return pruned_ts, pruned_idxList
 
 
-def exactLocalSearch(ts, m, dsr, absf_loc, bsf, idxList):
-
+def exactLocalSearch(ts, m, dsr, absf_loc, bsf, bsf_loc, idxList):
     offset = dsr - 1
-
     sub1 = ts[absf_loc[0] : min(absf_loc[0] + m, len(ts))]
     sub2 = ts[absf_loc[1] : min(absf_loc[1] + m, len(ts))]
-    original_bsf = np.sqrt(np.sum((sub1-sub2)**2))
-    bsf_locs = absf_loc
+    sub1 = ts[absf_loc[0] : absf_loc[0] + m]
+    sub2 = ts[absf_loc[1] : absf_loc[1] + m]
+    bsf_origin = round(np.sqrt(np.sum((sub1-sub2)**2)), 2)
 
     loc1_st, loc1_end = max(0, absf_loc[0] - offset), min(len(ts), absf_loc[0] + offset)
     loc2_st, loc2_end = max(0, absf_loc[1] - offset), min(len(ts), absf_loc[1] + offset)
@@ -43,18 +39,17 @@ def exactLocalSearch(ts, m, dsr, absf_loc, bsf, idxList):
         sub1 = ts[ii : min(ii + m, len(ts))]
         for jj in range(loc2_st , loc2_end):
             sub2 = ts[jj : min(jj + m, len(ts))]
-            temp_bsf = np.linalg.norm(sub1-sub2)
+            temp_bsf = round(np.linalg.norm(sub1-sub2),2)
             if temp_bsf <= bsf:
                 bsf = temp_bsf
-                bsf_locs = [idxList[ii], idxList[jj]]
+                bsf_loc = [idxList[ii], idxList[jj]]
 
-    return bsf, bsf_locs, original_bsf
+    return bsf, bsf_loc, bsf_origin
     
 
 
-def bsfMotif(mp, m):
+def bsfMotif(mp):
 
-    # mp = np.round(mp, 4)
     min_val = np.min(mp)
     min_indices = np.where(mp == min_val)[0]
     min_locations = [min_indices[0]]
@@ -66,27 +61,15 @@ def bsfMotif(mp, m):
 
 
 
-def findIndices(amp, m, absf, bsf):
+def findIndices(amp, m, absf, bsf, n):
+
     initset = np.where((amp >= absf) & (amp <= bsf))[0]
-    # candidates = find_ranges(initset, m)
-    candidates = np.array([np.arange(initset[ii], initset[ii] + m) \
-                            for ii in range(len(initset))])
-    candidates = np.concatenate(candidates)
-    candidates = np.unique(candidates, axis=0)
-    candidates = candidates[candidates < len(amp)]
+    candidates = np.concatenate([np.arange(init, init + m) for init in initset])
+    candidates = np.sort(pd.unique(candidates))
+    candidates = candidates[candidates < n]
+
     return candidates
 
-
-def find_ranges(arr, m):
-
-    # break_indices = np.where(np.diff(arr) != 1)[0]
-    # slices = np.split(arr, break_indices + 1)
-    # consecutive_ranges = np.array([np.arange(slice[0], slice[-1] + m) \
-    #                                for slice in slices])
-    consecutive_ranges = np.array([np.arange(arr[ii], arr[ii] + m) \
-                                   for ii in range(len(arr))])
-
-    return consecutive_ranges
 
 
 

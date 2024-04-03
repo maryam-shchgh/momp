538764e92db74a856311cc5fd5a7cfd5988c0425
Maryam <sshah073@ucr.edu>
2024-04-03 10:53:29 -0700

    MOMP MATLAB version is in progress

--- momp.m
+++ momp.m
@@ -1,13 +1,71 @@
 function [momp_out, mp_out] = momp(T, m, verbose)
-
+    n_orig = length(T);
     if verbose
-        fprintf('T is length %d, and m is set to %d\n', length(T), m);
+        fprintf('T is length %d, and m is set to %d\n', n_orig, m);
         [mp, ~, motifsIdx, ~] = mpx_v2(T, m, m);
         mp_out = motifsIdx(1:2,1);
     end
     
     %% downsamping the T and m
-    dsr = 8;
+    dsr = m / 4;
+    bsf = inf; refinedLoc = nan;
+    while dsr > 2
+        [amp, absf, bsf, prunedT] = process(T, m, dsr, bsf, refinedLoc, [1:length(T)]);
+        figure(1);
+        subplot(3,1,1)
+        plot(T)
+        subplot(3,1,2)
+        plot(amp, 'b');
+        hold on;
+        plot(mp, 'g');
+        yline(absf, 'r');
+        yline(bsf, 'g');
+        subplot(3,1,3)
+        plot(prunedT)
+        fprintf('T is length %d, and pruned is %d\n', length(T), length(prunedT));
+        input('Enter to continue')
+        T = prunedT;
+    end
+    momp_out = 0;
+%     Tds = paa(T, floor(length(T)/dsr));
+%     mds = floor(m/dsr);
+%     
+%     %% Computing the AMP and calculating the aBSF motifs
+%     [amp, ~, motifsIdx, ~] = mpx_v2(Tds, mds, mds);
+%     absfIdx = motifsIdx(1:2,1); % Best motif 
+%     absf = amp(absfIdx(1));
+%     amp = sqrt(dsr) * repelem(amp, dsr);
+%     absf = sqrt(dsr) * absf;
+%     absfIdx = dsr * absfIdx - 1;
+%     
+%     momp_out = 0;
+%     %% BSF local search
+%     bsf = bsfLocalSearch(T, m , dsr, absfIdx);    
+%     %% Prunnig
+%     filteredIdx = filterIndices(amp, absf, bsf, m, n_orig);
+%     prunedT = T(filteredIdx);  
+   
+%     if verbose
+%         figure(1);
+%         subplot(3,1,1)
+%         plot(T)
+%         subplot(3,1,2)
+%         plot(amp, 'b');
+%         hold on;
+%         plot(mp, 'g');
+%         yline(absf, 'r');
+%         yline(bsf, 'g');
+%         subplot(3,1,3)
+%         plot(prunedT)
+%         fprintf('T is length %d, and pruned is %d\n', length(T), length(prunedT));
+%     end
+    
+
+end
+
+
+function [amp, absf, bsf, prunedT] = process(T, m, dsr, bsf, refinedLoc, idxList)
+
     Tds = paa(T, floor(length(T)/dsr));
     mds = floor(m/dsr);
     
@@ -19,60 +77,59 @@ function [momp_out, mp_out] = momp(T, m, verbose)
     absf = sqrt(dsr) * absf;
     absfIdx = dsr * absfIdx - 1;
     
-    
     %% BSF local search
-    bsf = bsfLocalSearch(T, m , dsr, absfIdx);
-    
+    [bsf, refindLoc] = bsfLocalSearch(T, m , dsr, absfIdx, bsf, refinedLoc, idxList);    
     %% Prunnig
-    filteredIdx = filterIndices(amp, absf, bsf, m);
-    prunedT = T(filteredIdx);
-    [prunedmp, ~, motifsIdx, ~] = mpx_v2(prunedT, m, m);
-    momp_out = filteredIdx(motifsIdx(1:2,1));
-    figure(2); 
-    subplot(2,1,1); plot(T);
-    subplot(2,1,2); plot(prunedT);hold on, plot(prunedmp);
-    
-    
-    if verbose
-        figure(1);
-        plot(amp, 'b');
-        hold on;
-        plot(mp, 'g');
-        yline(absf, 'r');
-        yline(bsf, 'g');
-    end
-    
-    
-    
+    idxList = filterIndices(T, amp, absf, bsf, m);
+    prunedT = T(idxList);  
 
-    
 end
 
 
-function bsf = bsfLocalSearch(T, m , dsr, absfIdx)
+
+
+function [bsf, refinedLoc] = bsfLocalSearch(T, m , dsr, absfIdx, bsf, refinedLoc, idxList)
 
     offset = dsr  - 1;
-    width = 2*offset + m;
-    temp_T = zeros(1, width * length(absfIdx));
-    for ii = 1:length(absfIdx)
-        ll = absfIdx(ii);
-        temp_T((ii-1)*width + 1 : ii*width) = T(ll - offset + 1 : ll + offset + m);
+    range1 = (max(1,absfIdx(1) - offset) : min(length(T),absfIdx(1) + offset));
+    range2 = (max(1,absfIdx(2) - offset) : min(length(T),absfIdx(2) + offset));
+    for ii = range1
+        sub1 = T(ii:ii+m);
+        for jj = range2
+            sub2 = T(jj:jj+m);
+            [curr_bsf] = MASS_s2(sub1, sub2);
+            if curr_bsf <= bsf
+                bsf = curr_bsf;
+                refinedLoc = [idxList(ii), idxList(jj)];
+            end
+        end
     end
-    [mp, ~] = mpx(temp_T, m, m);
-    bsf = min(mp);
+    
+%     width = 2*offset + m;
+%     temp_T = zeros(1, width * length(absfIdx));
+%     for ii = 1:length(absfIdx)
+%         ll = absfIdx(ii);
+%         temp_T((ii-1)*width + 1 : ii*width) = T(ll - offset + 1 : ll + offset + m);
+%     end
+%     [mp, ~] = mpx(temp_T, m, m);
+%     bsf = min(mp);
 end
 
 
-function filteredIdx = filterIndices(amp, absf, bsf, m)
+function filteredIdx = filterIndices(T, amp, absf, bsf, m)
 
-    firstCandidates = find(amp >= absf & amp <= bsf);
-    filteredIdx = firstCandidates;
-%     filteredIdx = [];
-%     
-%     for ii = 1:length(firstCandidates)
-%         first = max(1, firstCandidates(ii) - m);
-%         last = min(firstCandidates(ii) + m, length(amp));
-%         filteredIdx(end:end+last-first + 1) = [filteredIdx, first:last];
-%     end
+    indices = find(amp >= absf & amp <= bsf);
+    % Generate indices for right neighbors
+    candidates = indices + (1:m);
+
+    % Clip indices to ensure they stay within bounds
+    candidates = min(length(T) - 1, candidates);
+
+    % Flatten the array and remove duplicates
+    filteredIdx = unique(candidates(:));
+
+    % Sort the result
+    filteredIdx = sort(filteredIdx);
+
 
 end

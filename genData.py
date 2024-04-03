3febca69b017095f247ac25518dc3a8553819f04
Maryam <sshah073@ucr.edu>
2024-04-03 16:01:21 -0700

    test data added for unit check purposes

--- genData.py
+++ genData.py
@@ -6,25 +6,35 @@ import matplotlib.pyplot as plt
 # %matplotlib widget
 
 def genData(dtype = 'rwalk'):
-    n = 64000
     np.random.seed(0)
-    t = np.arange(0, 10, 0.01)
-    motif = np.abs(np.sin(t))
-    # motif = motif + rnd.randn(len(motif))/20
-
+    
     if dtype == 'random':
+        n = 64000
+        t = np.arange(0, 10, 0.01)
+        motif = np.abs(np.sin(t))
         data = rnd.randn(n)/10
-        # t = np.arange(0, 10, 0.01)
-        # motif = np.abs(np.sin(t))/10
-        # motif = motif + rnd.randn(len(motif))/20
+        data[30000:30000+len(motif)] = motif
+        data[42000:42000+len(motif)] = motif
+        data = zscore(data + rnd.randn(len(data))/20)
         
 
     if dtype == 'rwalk':
+        n = 64000
+        t = np.arange(0, 10, 0.01)
+        motif = np.abs(np.sin(t))
         data = zscore(generate_random_walk(n))
+        data[30000:30000+len(motif)] = motif
+        data[42000:42000+len(motif)] = motif
+        data = zscore(data + rnd.randn(len(data))/20)
 
-    data[30000:30000+len(motif)] = motif
-    data[42000:42000+len(motif)] = motif
-    data = zscore(data + rnd.randn(len(data))/20)
+    if dtype == 'test':
+        n = 200
+        t = np.arange(0, 30, 1)
+        motif = np.abs(np.sin(t))
+        data = rnd.randn(n)/10
+        data[20:20+len(motif)] = motif
+        data[100:100+len(motif)] = motif
+        data = zscore(data + rnd.randn(len(data))/20)
     
     return data
 
@@ -40,9 +50,3 @@ def generate_random_walk(n):
     random_walk = np.insert(random_walk, 0, 0)
     
     return random_walk
-
-
-
-
-if __name__=="__main__":
-    genData(n=64000, dtype = 'rwalk')

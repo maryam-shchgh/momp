7ea33d28b8bded07403eb87f4cbb74096f0dcb1c
Maryam <sshah073@ucr.edu>
2024-04-04 23:49:38 -0700

    fixing bugs - still working on boudaries after pruning

--- momp.py
+++ momp.py
@@ -5,7 +5,7 @@ from numpy import random as rnd
 import matrixprofile as mpx
 import matplotlib.pyplot as plt
 from paa import paa
-import mass_ts as mts
+# import mass_ts as mts
 from plotAMP import plotResult
 from genData import genData
 from momp_utils import approxMP
@@ -13,47 +13,125 @@ from momp_utils import bsfMotif
 from momp_utils import prune
 from momp_utils import refine
 import warnings
-from scipy.stats import zscore
-import os
+# from scipy.stats import zscore
+# import os
 
-%matplotlib widget
+# %matplotlib widget
 
-def momp(T, m, verbose = True):
-    itertime = 0
+# def momp(T, m, verbose = True):
+#     itertime = 0
+#     bsf, bsf_loc = np.inf, None
+#     idxList = np.arange(len(T))
+#     dd = m // 16
+#     while True:
+#         st = time.time()
+#         T_input = T
+#         T, idxList, amp, absf, bsf, bsf_loc, bsf_origin, bsf_loc_origin = \
+#             process(T_input, m, dd, idxList, bsf, bsf_loc)
+#         pruning_perc = round(1- (len(T) / n_orig), 3)
+#         end = time.time()
+#         itertime += (end - st)
+#         if dd == 1:
+#             return   round(itertime,2) 
+         
+#         if verbose:
+#             print('MOMP: Tpaa1in{} |BSFdist : {} |Loc: {} |'\
+#                 'OriginBSFdist: {} |OriginBSFloc: {} |PrunePerc: {} |Time: {}'\
+#                 .format(dd, bsf, bsf_loc, bsf_origin, bsf_loc_origin,\
+#                         pruning_perc, round(end - st,2)))
+#             # plotResult(T_input, m, mp, amp, absf, bsf, bsf_loc, bsf_loc_origin, dd)
+        
+#         dd = dd // 2
+#         break
+
+
+
+def momp(T, m , verbose = True):
+
+    global T_origin
+    T_origin = T
+    dd = m // 16
     bsf, bsf_loc = np.inf, None
     idxList = np.arange(len(T))
-    dd = m // 4
-    while dd > 0.5:
-        st = time.time()
-        T_input = T
-        T, idxList, amp, absf, bsf, bsf_loc, bsf_origin, bsf_loc_origin = \
-            process(T_input, m, dd, idxList, bsf, bsf_loc)
-        pruning_perc = round(1- (len(T) / n_orig), 3)
-        end = time.time()
-        itertime += (end - st)
-        if verbose:
-            print('MOMP: Tpaa1in{} |BSFdist : {} |Loc: {} |'\
-                'OriginBSFdist: {} |OriginBSFloc: {} |PrunePerc: {} |Time: {}'\
-                .format(dd, bsf, bsf_loc, bsf_origin, bsf_loc_origin,\
-                        pruning_perc, round(end - st,2)))
-            # plotResult(T_input, m, mp, amp, absf, bsf, bsf_loc, bsf_loc_origin, dd)
+
+
+    print('T length: {} | m: {} | starting dsr:{}'.format(len(T), m, dd))
+    
+    while dd:
+
+        # Locating absf val/ loc using uamp
+        uamp = approxMP(T, m, dd)
+        absf, absf_loc = bsfMotif(uamp)
+        absf_loc_pruned_version = absf_loc
+        absf_loc = idxList[absf_loc]
+        # print('absf loc: ', absf_loc)
+
+        print('current idxList start - end :', idxList[0],'-',idxList[-1], '| len : ', len(idxList))
+        toprint = [{x: uamp[np.where(idxList == x)[0]]} for x in idxList if ((x > 290 and x < 310) or (x > 990 and x < 1010))] 
+        print('Boundary loc: ',toprint)
+        if 300 in idxList and 1001 in idxList : print('both motifs are in the boundaries')
+        else:
+            print('boundary error')
+
+        #plotting (sanity check)
+        fig1  = plt.figure(figsize=(4,3))
+        axs = fig1.subplots(2,1,sharex=True)
+        axs[0].plot(T)
+        for loc in absf_loc_pruned_version : axs[0].axvline(loc, color = 'r', label=  loc)
+        axs[1].plot(uamp)
+        axs[1].axhline(y=absf , color = 'r')
+        for loc in absf_loc_pruned_version : axs[1].axvline(loc, color = 'r', label = loc)
+        if dd == 1:
+            print('MOMP : Tpaa1in{} | BSF: {} | localBSF: {} | BSF loc: {} | localBSF loc: {} | Pruning : {}%'.\
+              format(dd, round(absf,2), round(absf,2), absf_loc, absf_loc, pruning))
+            return absf, absf_loc
+        # Refinement
         
+        bsf,bsf_loc, local_bsf = refine(T_origin,m, dd, absf_loc, bsf, bsf_loc)
+
+        #plotting (sanity check)
+        # print('local bsf: {}, bsf: {}'.format(local_bsf, bsf))
+        for loc in bsf_loc : axs[0].axvline(loc, color = 'g', label=  loc)
+        axs[1].axhline(y=bsf , color = 'g')
+        for ax in axs:
+            ax.legend()
+
+        # Pruning
+        pruned_T , pruned_idxList = prune(T, m, absf, bsf, uamp, idxList) 
+        # print('remaining indices: ',pruned_idxList)
+        fig2  = plt.figure(figsize=(4,2))
+        axs = fig2.subplots(1,1,sharex=True)
+        # axs[0].plot(T)
+        axs.plot(pruned_T, color = 'r')
+        plt.show()
+
+        pruning = round(1 - len(pruned_T) / len(T_origin), 2)
+
+        print('MOMP : Tpaa1in{} | BSF: {} | localBSF: {} | BSF loc: {} | localBSF loc: {} | Pruning : {}%'.\
+              format(dd, round(bsf,2), round(local_bsf,2), bsf_loc, absf_loc, pruning))
+        
+        T, idxList = pruned_T, pruned_idxList
         dd = dd // 2
 
-    return round(itertime,2)
         
 
-def process(T, m, dd, idxList, bsf, bsf_loc):
-    amp  = approxMP(T, m, dd)
-    absf, absf_loc= bsfMotif(amp)
-    if dd > 1:
-        bsf,bsf_loc, bsf_origin = refine(T,m, dd, absf_loc, bsf, bsf_loc, idxList)
-        bsf_loc_origin = idxList[absf_loc]
-        pruned_T , pruned_idxList = prune(T, m, absf, bsf, amp, idxList)
-        return pruned_T, pruned_idxList, amp, absf, bsf, bsf_loc, bsf_origin, bsf_loc_origin
-    else:
-        absf, absf_loc = round(absf,2), idxList[absf_loc]
-        return T, idxList, amp, absf, absf, absf_loc, absf, absf_loc
+
+# def process(T, m, dd, idxList, bsf, bsf_loc):
+#     amp  = approxMP(T, m, dd)
+#     absf, absf_loc= bsfMotif(amp)
+#     absf_loc = idxList[absf_loc]
+#     if dd > 1:
+#         # bsf,bsf_loc, bsf_origin = refine(T,m, dd, absf_loc, bsf, bsf_loc, idxList)
+#         bsf,bsf_loc, bsf_origin = refine(T_origin,m, dd, absf_loc, bsf, bsf_loc, idxList)
+#         if absf != bsf:
+#             pruned_T , pruned_idxList = prune(T, m, absf, bsf, amp, idxList)
+#         else:
+#             pruned_T, pruned_idxList = T, idxList
+#         return pruned_T, pruned_idxList, amp, absf, bsf, bsf_loc, bsf_origin, absf_loc
+#     else:
+#         # absf, absf_loc = round(absf,2), idxList[absf_loc]
+#         absf = round(absf,2)
+#         return T, idxList, amp, absf, absf, absf_loc, absf, absf_loc
 
 
 def next_closest_multiple_greater(N, K):
@@ -72,9 +150,11 @@ def main(verbose = True):
     plt.close('all')
     global n_orig
     global mp
+    global T_origin
 
-    T , m = genData(dtype='random') , 256
-    n, dd = len(T), m // 4
+    n = 100000
+    T , m = genData(n, dtype='rwalk') , 256
+    dd = m // 4
 
     if verbose:
         print('T length: {} | Requested motif length : {}'.format(n, m))
@@ -83,19 +163,22 @@ def main(verbose = True):
     padded_length = next_closest_multiple_greater(n, dd)
     padcount = padded_length - n
     T = np.pad(T, (0, padcount), mode='constant', constant_values=0)
-    n_orig = len(T)
+    T_origin, n_orig = T, len(T)
 
     #Evaluation of the result
-    st = time.time()
-    mp = mpx.compute(T,m)['mp']
-    _, min_loc = bsfMotif(mp)
-    mp_time = round(time.time() - st,2)
+    # st = time.time()
+    # mp = mpx.compute(T,m)['mp']
+    # _, min_loc = bsfMotif(mp)
+    # mp_time = round(time.time() - st,2)
+
+    # fig = plt.figure(figsize=(4,2))
+    # axs = fig.subplots(2,1,sharex=True)
+    # axs[0].plot(T)
+    # axs[1].plot(mp)
     if verbose:
         print('MPx: {} | minval : {}'.format(min_loc, round(np.min(mp),2)))
 
-    # st = time.time()
     momp_time = momp(T, m, verbose)
-    # momp_time = round(time.time() - st,2)
 
     if verbose:
         print('Speedup : {}X | MPx:{}s | MOMP: {}s'.format(round(mp_time / momp_time), mp_time, momp_time))
--- momp_utils.py
+++ momp_utils.py
@@ -8,50 +8,63 @@ import time
 import matplotlib.pyplot as plt
 
 
-
 def approxMP(ts, m, dsr):
-    if dsr > 1 : 
-        ts_dsmp = paa(ts, len(ts)//dsr)
-        w_dsmp = np.ceil(m/dsr).astype(int)
-        dsmp_mp = mpx.compute(ts_dsmp, w_dsmp)['mp']
-        amp = np.sqrt(dsr) * np.repeat(dsmp_mp, dsr)
-        return amp
-    else:
-        return mpx.compute(ts,m)['mp']
+
+    ts_dsmp = paa(ts, len(ts)//dsr)
+    w_dsmp = np.ceil(m/dsr).astype(int)
+    dsmp_mp = mpx.compute(ts_dsmp, w_dsmp)['mp']
+    amp = np.sqrt(dsr) * np.repeat(dsmp_mp, dsr)
+
+    # fig = plt.figure(figsize = (4,3))
+    # plt.plot(amp)
+
+    return amp
 
 def bsfMotif(mp):
-
     min_val = np.min(mp)
-    min_indices = np.where(mp == min_val)[0]
-    min_locations = [min_indices[0]]
-    for ii in range(1, len(min_indices)):
-        if min_indices[ii] != min_indices[ii-1] + 1:
-            min_locations.append(min_indices[ii])
+    # print('absf mp min val: ', min_val)
+    # print('absf mp vals: ', mp)
+    indices = np.where(mp == min_val)[0]
+    # print('absf equal indices: ', indices)
+    start_indices = np.where(np.diff(np.insert(indices, 0, -2)) > 1)[0]
+    # motif_loc = idxList[indices[start_indices]]
+    motif_loc = indices[start_indices]
+    # min_locations = [min_indices[0]]
+    # for ii in range(1, len(min_indices)):
+    #     if min_indices[ii] != min_indices[ii-1] + 1:
+    #         min_locations.append(min_indices[ii])
 
-    return min_val, min_locations
+    return min_val, motif_loc
 
 
-def refine(ts, m, dsr, absf_loc, bsf, bsf_loc, idxList):
+def refine(ts, m, dsr, absf_loc, bsf, bsf_loc):
     offset = dsr - 1
-    sub1 = ts[absf_loc[0] : min(absf_loc[0] + m, len(ts))]
-    sub2 = ts[absf_loc[1] : min(absf_loc[1] + m, len(ts))]
+    # sub1 = ts[absf_loc[0] : min(absf_loc[0] + m, len(ts))]
+    # sub2 = ts[absf_loc[1] : min(absf_loc[1] + m, len(ts))]
     sub1 = ts[absf_loc[0] : absf_loc[0] + m]
     sub2 = ts[absf_loc[1] : absf_loc[1] + m]
-    bsf_origin = round(np.sqrt(np.sum((sub1-sub2)**2)), 2)
+    # local_bsf = round(np.sqrt(np.sum((sub1-sub2)**2)), 2)
+    temp_mp = mpx.compute(np.concatenate((sub1, sub2)),m)['mp']
+    local_bsf , _ = bsfMotif(temp_mp)
 
-    loc1_st, loc1_end = max(0, absf_loc[0] - offset), min(len(ts), absf_loc[0] + offset)
-    loc2_st, loc2_end = max(0, absf_loc[1] - offset), min(len(ts), absf_loc[1] + offset)
+    loc1_st, loc1_end = max(0, absf_loc[0] - offset), min(len(ts) - m, absf_loc[0] + offset)
+    loc2_st, loc2_end = max(0, absf_loc[1] - offset), min(len(ts) - m, absf_loc[1] + offset)
 
     for ii in range(loc1_st , loc1_end):
-        sub1 = ts[ii : min(ii + m, len(ts))]
+        sub1 = ts[ii : ii + m]
         for jj in range(loc2_st , loc2_end):
-            sub2 = ts[jj : min(jj + m, len(ts))]
-            temp_bsf = round(np.linalg.norm(sub1-sub2),2)
+            sub2 = ts[jj : jj + m]
+            # temp_bsf = round(np.linalg.norm(sub1-sub2),2)
+            temp_mp = mpx.compute(np.concatenate((sub1, sub2)),m)['mp']
+            temp_bsf , _ = bsfMotif(temp_mp)
+            # temp_bsf = round(np.sqrt(np.sum((sub1-sub2)**2)), 2)
+            # print('[{}:{}] and [{}:{}]  -> {}'.format(ii, ii+m, jj , jj +m, temp_bsf))
             if temp_bsf <= bsf:
                 bsf = temp_bsf
-                bsf_loc = [idxList[ii], idxList[jj]]
+                # bsf_loc = [idxList[ii], idxList[jj]]
+                bsf_loc = [ii, jj]
 
-    return bsf, bsf_loc, bsf_origin
+    return bsf, bsf_loc, local_bsf
 
 
 def prune(T, m, absf, bsf, amp, idxList):
@@ -61,7 +74,8 @@ def prune(T, m, absf, bsf, amp, idxList):
 
 
 def findIndices(amp, m, absf, bsf, n):
-
+    print('amp in pruning: ', amp)
+    print('boundaries in pruning: ', absf, bsf)
     initset = np.where((amp >= absf) & (amp <= bsf))[0]
     split_indices = np.where(np.diff(initset) != 1)[0] + 1
     subarrays = np.split(initset, split_indices)

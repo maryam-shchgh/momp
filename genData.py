dea483bcd7d115b04ae53c6518ce3f8d1d6691ed
Maryam <sshah073@ucr.edu>
2024-04-08 01:25:05 -0700

    random walk updated

--- genData.py
+++ genData.py
@@ -12,46 +12,31 @@ def genData(n, dtype = 'rwalk'):
         n = 64000
         t = np.arange(0, 10, 0.01)
         motif = np.abs(np.sin(t))
-        data = rnd.randn(n)/10
-        data[30000:30000+len(motif)] = motif
-        data[42000:42000+len(motif)] = motif
-        data = zscore(data + rnd.randn(len(data))/20)
+        T = rnd.randn(n)/10
+        T[30000:30000+len(motif)] = motif
+        T[42000:42000+len(motif)] = motif
+        T = zscore(T + rnd.randn(len(T))/20)
         
 
     if dtype == 'rwalk':
-        # n = 64000
-        # t = np.arange(0, 10, 0.01)
-        # motif = np.abs(np.sin(t))
-        # data = zscore(generate_random_walk(n))
-        # data[30000:30000+len(motif)] = motif
-        # data[42000:42000+len(motif)] = motif
-        # data = zscore(data + rnd.randn(len(data))/20)
-        data = generate_random_walk(n) /10
-        data[10000:10300]  = np.abs(np.sin(np.arange(0,30,0.1))) /4
-        data[40000:40300]  = np.abs(np.sin(np.arange(0,30,0.1)))/ 4
-        data = zscore(data + np.random.rand(len(data))/20)
+        T = generate_random_walk(n) /10
+        T[30000:30600]  = np.abs(np.sin(np.arange(0,6,0.01))) /4
+        T[60000:60600]  = np.abs(np.sin(np.arange(0,6,0.01)))/ 4
+        T = zscore(T + np.random.rand(len(T))/20)
 
     if dtype == 'test':
         t = np.arange(0, 30, 1)
         motif = np.abs(np.sin(t))
-        data = rnd.randn(n)/10
-        data[20:20+len(motif)] = motif
-        data[100:100+len(motif)] = motif
-        data = zscore(data + rnd.randn(len(data))/20)
+        T = rnd.randn(n)/10
+        T[20:20+len(motif)] = motif
+        T[100:100+len(motif)] = motif
+        T = zscore(T + rnd.randn(len(T))/20)
     
-    return data
+    return T
 
 
 def generate_random_walk(n):
     np.random.seed(0)
-    # # Generate random increments (+1 or -1) for each step
-    # increments = np.random.choice([-1, 1], size=n-1)
-    
-    # # Calculate the cumulative sum of the increments to get the random walk
-    # random_walk = np.cumsum(increments)
-    
-    # # Add starting point (0) to the random walk
-    # random_walk = np.insert(random_walk, 0, 0)
     # Probability to move up or down
     prob = [0.05, 0.95]  
     

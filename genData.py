55b9900a7d1213f0968c881f016aa6a11c6515f0
Maryam <sshah073@ucr.edu>
2024-04-08 18:01:57 -0700

    simple randomwalk added for test purposes

--- genData.py
+++ genData.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # %matplotlib widget
 
-def genData(n, dtype = 'rwalk'):
+def genData(n, mlen, dtype = 'rwalk'):
     np.random.seed(0)
     
     if dtype == 'random':
@@ -20,8 +20,11 @@ def genData(n, dtype = 'rwalk'):
 
     if dtype == 'rwalk':
         T = generate_random_walk(n) /10
-        T[30000:30600]  = np.abs(np.sin(np.arange(0,6,0.01))) /4
-        T[60000:60600]  = np.abs(np.sin(np.arange(0,6,0.01)))/ 4
+        # ii, jj = round(0.3*len(T)) , round(0.7*len(T))
+        ii, jj = 32, 100
+        # T[ii : ii + mlen]  = np.abs(np.sin(np.arange(0,mlen*0.01,0.01))) /4
+        T[ii : ii + mlen] = np.concatenate((np.arange(0,2,0.25), np.arange(2, 0, -0.25)))
+        T[jj : jj + mlen]  = T[ii : ii + mlen]
         T = zscore(T + np.random.rand(len(T))/20)
 
     if dtype == 'test':
@@ -44,16 +47,14 @@ def generate_random_walk(n):
     start = 2 
     random_walk = [start]
     
-    # creating the random points
-    rr = np.random.random(n)
-    downp = rr < prob[0]
-    upp = rr > prob[1]
-    
-    
-    for idownp, iupp in zip(downp, upp):
-        down = idownp and random_walk[-1] > 1
-        up = iupp and random_walk[-1] < 4
-        random_walk.append(random_walk[-1] - down + up)
+    for _ in range(n-1):  # Loop n-1 times to generate a time series of length n
+        rr = np.random.random()  # Generate a single random number
+        if rr < prob[0] and random_walk[-1] > 1:
+            random_walk.append(random_walk[-1] - 1)  # Move down
+        elif rr > prob[1] and random_walk[-1] < 4:
+            random_walk.append(random_walk[-1] + 1)  # Move up
+        else:
+            random_walk.append(random_walk[-1])  # Stay in the same position
     
     random_walk = np.array(random_walk)
     return random_walk

83c4f256610bac098883808463f25a1152dd418b
Maryam <sshah073@ucr.edu>
2024-04-23 23:05:22 -0700

    KTIP written based on mpx

--- ktip_v1.m
+++ ktip_v1.m
@@ -5,24 +5,6 @@
 %%
 function [kTriangIneqProfile] = ktip_v1(timeSeries, subseqLen, dsrate, minlag)
 
-% Code and update formulas are by Kaveh Kamgar.  
-% GUI and top k motif critera are based on some code by Michael Yeh. 
-% The suggested use of the fourier transform to compute euclidean distance based on cross correlation 
-% is from Abdullah Mueen. 
-%
-% Additional References
-% Yan Zhu, et al, Matrix Profile II: Exploiting a Novel Algorithm and GPUs to break the one Hundred Million Barrier for Time Series Motifs and Join
-% Zachary Zimmerman, et al, Scaling Time Series Motif Discovery with GPUs: Breaking the Quintillion Pairwise Comparisons a Day Barrier. (pending review)
-% Philippe Pebay, et al, Formulas for Robust, One-Pass Parallel Computation of Covariances and Arbitrary-Order Statistical Moments
-% Takeshi Ogita, et al, Accurate Sum and Dot Product
-
-
-% difference equations have 0 as their first entry here to simplify index
-% calculations slightly. Alternatively, it's also possible to swap this to the last element
-% and reorder the comparison step (or omit on the last step). This is a
-% special case when comparing a single time series to itself. The more general
-% case with time series A,B can be computed using difference equations for
-% each time series.
 
 if nargin < 2
     error('incorrect number of input arguments');
@@ -37,8 +19,6 @@ if ~exist('minlag', 'var')
     minlag = 0;
 end
 
-% selfjoin = 1;
-% timeSeries  = timeSeriesA;
 
 
 n = length(timeSeries);
@@ -60,7 +40,6 @@ df = [0; (1/2)*(timeSeries(1 + subseqLen : n) - timeSeries(1 : n - subseqLen))];
 dg = [0; (timeSeries(1 + subseqLen : n) - mu(2 : n - subseqLen + 1)) + (timeSeries(1 : n - subseqLen) - mu(1 : n - subseqLen))];
 
 % Output Similarity matrix
-similarityMatrixLength = n - subseqLen + 1;
 profileRowCount = n - subseqLen + 1;
 
 % similarityMatrix = NaN(similarityMatrixLength, similarityMatrixLength);
@@ -71,15 +50,12 @@ kTriangIneqProfile = NaN(profileRowCount, 1);
 % This uses normalized cross correlation as an intermediate quantity for performance reasons. 
 % It is later reduced to z-normalized euclidean distance.
 
-% for diag = minlag + 1 : n - subseqLen + 1
 for diag = minlag + 1 : dsrate
     cov_ = (sum((timeSeries(diag : diag + subseqLen - 1) - mu(diag)) .* (timeSeries(1 : subseqLen) - mu(1))));
     for row = 1 : n - subseqLen - diag + 2
         col = row + diag - 1;
         cov_ = cov_ + df(row) * dg(col) + df(col) * dg(row);
         corr_ = cov_ * invsig(row) * invsig(col);
-%         similarityMatrix(row, col) = corr_;
-%         similarityMatrix(col, row) = corr_;
         if isnan(kTriangIneqProfile(row)) || corr_ < kTriangIneqProfile(row)
             kTriangIneqProfile(row) = corr_;
         end
@@ -91,8 +67,6 @@ for diag = minlag + 1 : dsrate
     end
 end
 
- 
-% similarityMatrix = sqrt(max(0, 2 * (subseqLen - similarityMatrix), 'includenan'));
 kTriangIneqProfile = sqrt(max(0, 2 * (subseqLen - kTriangIneqProfile), 'includenan'));
 
 if transposed_   % matches the profile and profile index but not the motif or discord index to the input format
